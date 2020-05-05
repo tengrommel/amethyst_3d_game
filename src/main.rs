f80af8231ca04e6604179a4103baf3e8add3600d
teng <wwwsousoucom@126.com>
2020-05-06 01:47:24 +0800

    simple state

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ use amethyst::{
 };
 
 struct GameplayState {
-    play_count: u8,
+    lifeforms: u8,
 }
 
 struct PausedState;
 
 // GameData is the internal shared data between states
-impl State<GameData<'static, 'static>, ()> for GameplayState {
+impl SimpleState for GameplayState {
     fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        println!("Number of players: {}", self.play_count);
+        println!("Number of lifeforms: {}", self.lifeforms);
     }
 }
 
@@ -26,5 +26,11 @@ fn main() -> amethyst::Result<()> {
     // amethyst::start_logger(Default::default());
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let game_data = GameDataBuilder::default();
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
+    let mut game = Application::new(assets_dir,
+                                    GameplayState{lifeforms:0}, game_data)?;
+    game.run();
     Ok(())
 }
