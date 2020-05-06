c2dedf599c9ed3468e36b049d132a4484ae1e70d
teng <wwwsousoucom@126.com>
2020-05-06 17:18:15 +0800

    windows ok

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate amethyst;
-
 use amethyst::{
     prelude::*,
+    //renderer is used to display a window
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
     },
+    //needed for application_root_dir() etc
     utils::application_root_dir,
 };
 
@@ -14,9 +15,7 @@ struct GameplayState {
     lifeforms: u8,
 }
 
-struct PausedState;
-
-// GameData is the internal shared data between states
+//GameData is the internal shared data between states
 impl SimpleState for GameplayState {
     fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
         println!("Number of lifeforms: {}", self.lifeforms);
@@ -34,13 +33,12 @@ fn main() -> amethyst::Result<()> {
                     RenderToWindow::from_config_path(display_config_path)?
                         .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
-                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderFlat2D::default()),
         )?;
 
     let assets_dir = app_root.join("assets");
-    let mut world = World::new();
     let mut game = Application::new(assets_dir,
-                                    GameplayState { lifeforms: 0 }, game_data)?;
+                                    GameplayState{lifeforms:0}, game_data)?;
     game.run();
     Ok(())
 }
