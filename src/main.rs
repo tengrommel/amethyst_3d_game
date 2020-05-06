afb42e788e5438a5e31e246b1dcb3514b5065629
teng <wwwsousoucom@126.com>
2020-05-06 13:59:29 +0800

    bug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate amethyst;
+
 use amethyst::{
     prelude::*,
     renderer::{
@@ -23,14 +24,23 @@ impl SimpleState for GameplayState {
 }
 
 fn main() -> amethyst::Result<()> {
-    // amethyst::start_logger(Default::default());
+    amethyst::start_logger(Default::default());
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
-    let game_data = GameDataBuilder::default();
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default())
+        )?;
+
     let assets_dir = app_root.join("assets");
     let mut world = World::new();
     let mut game = Application::new(assets_dir,
-                                    GameplayState{lifeforms:0}, game_data)?;
+                                    GameplayState { lifeforms: 0 }, game_data)?;
     game.run();
     Ok(())
 }
