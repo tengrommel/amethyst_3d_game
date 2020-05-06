2c3f083c0181205f1d8b8915f33ff6f566f64a78
teng <wwwsousoucom@126.com>
2020-05-06 17:56:58 +0800

    we initialise camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate amethyst;
 use amethyst::{
+    core::transform::Transform,
     prelude::*,
     //renderer is used to display a window
     renderer::{
+        Camera,
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
@@ -11,14 +13,31 @@ use amethyst::{
     utils::application_root_dir,
 };
 
+pub const ARENA_HEIGHT: f32 = 100.00;
+pub const ARENA_WIDTH: f32 = 100.00;
+
 struct GameplayState {
     lifeforms: u8,
 }
 
+fn initialise_camera(world: &mut World) {
+    // Setup camera in a way our screen covers whole arena and (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    // Camera position
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
+
 //GameData is the internal shared data between states
 impl SimpleState for GameplayState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         println!("Number of lifeforms: {}", self.lifeforms);
+        let world = data.world;
+        initialise_camera(world);
     }
 }
 
